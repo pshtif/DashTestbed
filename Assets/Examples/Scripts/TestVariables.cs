b40a7468534dd5bb483bd793fdea71c57ebe00ef
sHTiF <pshtif@gmail.com>
2022-01-14 13:23:45 +0100

    update testbed

---
+++ Assets/Examples/Scripts/TestVariables.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Dash;
+using UnityEngine;
+
+public class TestVariables : DashVariablesController
+{
+    public float test = 5;
+}
